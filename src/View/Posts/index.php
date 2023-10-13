f423995f504de8565e0564913301d95cd3d91739
okubokosei95 <147782039+okubokosei95@users.noreply.github.com>
2023-10-13 09:24:02 +0000

    スペースの削除

--- src/View/Posts/index.php
+++ src/View/Posts/index.php
@@ -20,7 +20,6 @@
 <body>
     <div class="navbar header">
         <span class = "navbar-brand mb-0 h1"><?= $this->get('serviceName') ?></span>
-        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
